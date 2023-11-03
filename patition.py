d600d37362e7ce6e2aeac53594ecb7390ef3ce72
B.Mohith Siva Sai <92243592+B-mohith@users.noreply.github.com>
2023-11-03 16:17:52 +0530

    Update patition.py

--- patition.py
+++ patition.py
@@ -1,3 +1,27 @@
+import torch
+import torch.nn as nn
+from torch.nn import init
+from torch.autograd import Variable
+import pickle
+import numpy as np
+import time
+import random
+from collections import defaultdict
+from Node_Encoders import Node_Encoder
+from Node_Aggregators import Node_Aggregator
+import torch.nn.functional as F
+import torch.utils.data
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from math import sqrt
+import datetime
+import argparse
+import os
+import sys
+from GraphConsis import GraphConsis
+import networkx as nx
+import matplotlib.pyplot as plt
+
 def main():
   parser = argparse.ArgumentParser(description='Social Recommendation: GraphConsis model')
   parser.add_argument('--batch_size', type=int, default=128, metavar='N', help='input batch size for training')
@@ -21,3 +45,43 @@ def main():
 
   history_u_lists, history_ur_lists, history_v_lists, history_vr_lists, traindata, validdata, testdata, social_adj_lists, item_adj_lists, ratings_list = pickle.load(
     data_file)
+  G = nx.Graph()
+
+# Add the users and items as nodes to the graph.
+ for u in history_u_lists:
+    G.add_node(u, type='user')
+
+ for v in history_v_lists:
+    G.add_node(v, type='item')
+
+# Add the edges to the graph.
+ for u, ur_list in history_ur_lists.items():
+    for i in ur_list:
+        G.add_edge(u, i, weight=1)
+
+ for v, vr_list in history_vr_lists.items():
+    for r in vr_list:
+        G.add_edge(r, v, weight=1)
+
+ pos = nx.fruchterman_reingold_layout(G)
+
+# Draw the nodes and edges of the graph.
+ nx.draw_networkx_nodes(G, pos, node_size=50, node_color='blue', alpha=0.5)
+ nx.draw_networkx_edges(G, pos, edge_color='black', alpha=0.3)
+
+# Add labels to the nodes.
+ for node, attr in G.nodes(data=True):
+    if attr['type'] == 'user':
+        label = 'User ' + node
+    else:
+        label = 'Item ' + node
+
+    nx.draw_networkx_labels(G, pos, labels={node: label}, horizontalalignment='center', verticalalignment='center')
+
+# Set the plot title and axis labels.
+ plt.title('Ciao Dataset Graph')
+ plt.xlabel('Users')
+ plt.ylabel('Items')
+
+# Show the plot.
+ plt.show()
